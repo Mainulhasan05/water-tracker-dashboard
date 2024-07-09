660c59a92ca2074768c07aef13d6491949bcc950
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-07-09 19:01:26 +0600

    update

--- Components/Source/SourceHome.js
+++ Components/Source/SourceHome.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import axiosInstance from "../../utils/axiosInstance";
 import SourceCardItem from "./SourceCardItem";
+import { useRouter } from "next/router";
 
 const SourceHome = ({ data }) => {
+  const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 18; //
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    router.push(`?page=${page}`, undefined, { shallow: true });
+  };
+
+  const totalPages = Math.ceil(data?.total / 18); // Calculate total pages based on total data
+
   return (
     <main id="main">
       <h2>Working on it {data?.data?.length}</h2>
@@ -13,6 +24,45 @@ const SourceHome = ({ data }) => {
           </div>
         ))}
       </div>
+      <nav>
+        <ul className="pagination justify-content-center">
+          <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+            <button
+              className="page-link"
+              onClick={() => handlePageChange(currentPage - 1)}
+            >
+              Previous
+            </button>
+          </li>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <li
+              key={index}
+              className={`page-item ${
+                currentPage === index + 1 ? "active" : ""
+              }`}
+            >
+              <button
+                className="page-link"
+                onClick={() => handlePageChange(index + 1)}
+              >
+                {index + 1}
+              </button>
+            </li>
+          ))}
+          <li
+            className={`page-item ${
+              currentPage === totalPages ? "disabled" : ""
+            }`}
+          >
+            <button
+              className="page-link"
+              onClick={() => handlePageChange(currentPage + 1)}
+            >
+              Next
+            </button>
+          </li>
+        </ul>
+      </nav>
     </main>
   );
 };
